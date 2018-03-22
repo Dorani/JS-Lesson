0919a1fc4466abda30f630e44d05f6740752e707
Seif <seif.dorani@gmail.com>
2018-03-22 14:39:26 -0700

    explanation on coersion, variables, data types

---
+++ JS Section 3/var2.js
@@ -0,0 +1,28 @@
+//Variables
+
+var name = 'John';
+var age = 26;
+
+//now let's print these 2 together on the console
+
+console.log(name + age);
+
+//add numbers and strings together with the plus sign
+
+//converts number into a strings
+//diff data types mixed together
+
+//type coersion: js converts some data types to another on the fly when it needs it
+
+console.log(age + age);
+//it will simply add these numbers
+//output in blue
+
+var job, isMarried;
+console.log(job);
+//undefined
+
+job = 'teacher';
+isMarried = false;
+
+console.log(name + ' is a ' + age + ' year old' + job + ' is he married? ' + isMarried + '.');
