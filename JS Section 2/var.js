0dc9ede7e2d44e9ca62c0b2dc0262e5e044b6866
Seif <seif.dorani@gmail.com>
2018-03-22 12:40:36 -0700

    data types explanation

--- JS Section 2/var.js
+++ JS Section 2/var.js
@@ -1,3 +1,30 @@
 var name = 'John'; //string go in between quotes
 console.log(name);//we can use the name variable
 //it will retrieve the value that is stored in that variable
+//when you inspect element and go to console you can see the line it came from in our file
+//very useful later
+
+
+//created very 1st var!
+
+//let's declare our next var!
+
+
+var lastName = 'Smith';// when we declare a var name that has 2 parts, we use camel charset
+console.log(lastName);
+
+//3rd variable
+
+var age = 26;//did not use quotes, not a string, number/integer
+console.log(age);
+
+//data types:
+    //Primitive data types: these are not objects, simple data types
+
+
+//numbers strings boolean undefined null
+//numbers: floating points numbers, dec + integer
+//string: sequence of characters, used for text
+//boolean: logical data type t or f
+//undefined: data type of var which does not have a value yet
+//null: also means non existent
