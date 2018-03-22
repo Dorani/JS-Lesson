f552f68eb2d12f7b9539506ede09fb4e04c2796c
Seif <seif.dorani@gmail.com>
2018-03-22 15:03:05 -0700

    ops

--- JS Section 4/ops.js
+++ JS Section 4/ops.js
@@ -1,2 +1,12 @@
 var birthYear = 2016 - 26;
+
+birthYear = 2018 - 26 * 2;
+//we are repeating things here no?
+
+//what can we do?
+var now = 2018;
+birthYear = now - 26 * 2;
 console.log(birthYear)
+
+//js does the multiplication
+//then the result from 2016, which is now
