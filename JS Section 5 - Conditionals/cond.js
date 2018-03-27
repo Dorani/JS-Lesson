f3fe974f2af51219c3a35894fa5003710f0fb60d
Seif <seif.dorani@gmail.com>
2018-03-26 22:29:02 -0700

    1st example

--- JS Section 5 - Conditionals/cond.js
+++ JS Section 5 - Conditionals/cond.js
@@ -1,1 +1,12 @@
-console.log("live")
+//Conditionals:
+// If-Else statements
+
+var age = 'John';
+var age = 26;
+var isMarried = 'no';
+
+if (isMarried === 'yes'){
+  console.log(name + ' is married');
+} else {
+  console.log(name + ' will hopefully marry soon :)');
+}
