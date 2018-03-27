c37ea7eeb54f963f14eb5a6a2129df8be8b70a7e
Seif <seif.dorani@gmail.com>
2018-03-26 22:32:30 -0700

    fixed typo and comments on exmaples provided"

--- JS Section 5 - Conditionals/cond.js
+++ JS Section 5 - Conditionals/cond.js
@@ -1,12 +1,17 @@
 //Conditionals:
 // If-Else statements
 
-var age = 'John';
+var name = 'John';
 var age = 26;
 var isMarried = 'no';
 
-if (isMarried === 'yes'){
-  console.log(name + ' is married');
-} else {
-  console.log(name + ' will hopefully marry soon :)');
+if (isMarried === 'yes'){//the condition needs to be evaluated to true so that:
+  console.log(name + ' is married');//this gets executed
+} else { // and if its false
+  console.log(name + ' will hopefully marry soon :)');//this gets executed
 }
+
+// comparason operator === returns a boolean T or F value
+// isMarried is set to yes?
+// it evaluates to false
+// the else statement is returned.
