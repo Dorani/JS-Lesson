eb5b8b9e81faf4a320fd3f32c44301a15154a097
Seif <seif.dorani@gmail.com>
2018-03-26 22:53:29 -0700

    boolean logic explanatipn

--- Section 6 - Boolean Logic and Switch/bool.js
+++ Section 6 - Boolean Logic and Switch/bool.js
@@ -7,3 +7,17 @@ if (age < 20){
 } else {
   console.log('John is a man');
 }
+
+//go over boolean logic
+ age >= 20;
+ //false
+ age < 30;
+ true
+ !(age < 30);
+ //false
+
+
+ age >= 20 && age < 30;// and operator rules - because they are not both true
+ //false
+ age >= 20 || age < 30;//or operator results when just one is true
+ //true
