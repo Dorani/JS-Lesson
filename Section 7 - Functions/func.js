54f2b93fca9948c029b49b48c89c9c7e67fdb92f
Seif <seif.dorani@gmail.com>
2018-03-26 23:27:26 -0700

    explanation on functions"

--- Section 7 - Functions/func.js
+++ Section 7 - Functions/func.js
@@ -1,1 +1,26 @@
-console.log('funco');
+//Functions:
+
+//piece of code we want to run over and over
+//functions are like containers that hold lines of code
+//we pass arguments in them
+//and then output a result
+
+
+function calculateAge(yearOfBirth){
+  var age = 2018 - yearOfBirth;
+  return age;
+}
+var ageJohn = calculateAge(1990);
+console.log(ageJohn);
+
+
+//keyword function and name of function, pass in argument = data we want to pass into the functions
+//function body
+  // define a variable age and make it equal to 2018 - argument passed inspect
+  // return the age
+
+//to call the function, we need to write it out and pass in information
+// we pass in 1990
+// but this value will not be stored anywhere so:
+
+// we create a variable for it, ageJohn and we call the function with any argument we want for that specific person
