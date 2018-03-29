62d4d50864775ad6a0b0f1993fb4be10e69c6596
Seif <seif.dorani@gmail.com>
2018-03-29 00:01:01 -0700

    the diff between expressions and statements

---
+++ Section 7 - Functions/statementsAndExpressions.js
@@ -0,0 +1,16 @@
+//Statements and expressions
+
+
+//statement
+function someFun(stuff){
+  //code
+}
+
+
+//expression
+var someFun = function(stuff){
+  //code
+}
+
+//the difference is that an expressions produces a value/outcome
+//a statement performs an action
