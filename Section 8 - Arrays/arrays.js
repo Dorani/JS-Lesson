c87b0c5d02880ca077a75682dce7bbbaed7da184
Seif <seif.dorani@gmail.com>
2018-03-29 00:09:57 -0700

    arrays in action with explanation

---
+++ Section 8 - Arrays/arrays.js
@@ -0,0 +1,20 @@
+//Arrays:
+
+
+//We sometimes had diff variables for diff people
+//If we can bundle these into one single variable that would be awesome....
+  //we can:
+
+var names = ['john', 'jane', 'mark'];
+//before we made 3 different variables
+//instead we have an array with 3 strings
+
+var years = [1999, 1969, 1948];
+
+//Now to access the values in the array we do the following:
+//we use the [] and put the order number of the element we want to retrive
+//In JS arrays are 0 based:
+  //1st element has index number of 0
+
+console.log(name[0]);
+//john
