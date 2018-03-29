63d33d7c85b3474346d55882b8f690f91d6cf835
Seif <seif.dorani@gmail.com>
2018-03-29 00:39:59 -0700

    pbjects, and examples

--- Section 9 - Objects/obj.js
+++ Section 9 - Objects/obj.js
@@ -1,1 +1,21 @@
 //Objects
+
+//With arrays, we can access elements of a certain array, by order number
+//But what if we wanted to access elements of an array by a specific name or keyword
+//You could't do that, unless you use objects.
+
+
+//Objects have key value pairs. Used to group variables that belong together with NO
+//particular order
+
+//object literal example:
+
+var person = {
+  name: 'john',
+  lastName: 'smith',
+  year: 1990,
+  job: 'teacher',
+  isMarried: false
+};
+console.log(person);
+//all of these key value pairs as an indivisual variable inside the John object:
