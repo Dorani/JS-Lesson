406f40747ce0e416b269789a556701659b24eec1
Seif <seif.dorani@gmail.com>
2018-03-29 00:46:15 -0700

    more on obj

--- Section 9 - Objects and Properties/obj.js
+++ Section 9 - Objects and Properties/obj.js
@@ -19,3 +19,22 @@ var person = {
 };
 console.log(person);
 //all of these key value pairs as an indivisual variable inside the John object:
+
+//now to retrieve an element inside of an object:
+
+// dot notation
+
+console.log(john.lastName);
+//smith
+//or this way
+console.log(john['lastName']);
+
+
+//Data mutation:
+john.lastName = 'miller';
+john['job'] = ['software'];
+
+console.log(john);
+//data mutation works in both ways just like arrays
+
+//---------------------
