06a42717d3647bdb1947a6273f70ac2f1b8da7f9
Seif <seif.dorani@gmail.com>
2018-03-28 23:34:03 -0700

    more explanation on functions

--- Section 7 - Functions/func.js
+++ Section 7 - Functions/func.js
@@ -32,6 +32,13 @@ console.log(ageMarry);
 
 // we create a variable for it, ageJohn and we call the function with any argument we want for that specific person
 
+  //js sees this line 16 and simply stores this function in memory and waits so we use it
+  //that means: calculateAge, we call the function, we pass in 1990, which is the argument stored as yearOfBirth
+  //this is then used in the function body and stored into the age variable
+  //which is finaly returned
+  //so: calculateAge(1990)=returns the age which is ultimately assigned to a person
+  //this was made to show how we can execute one function for multiple people and store it!
+
 //the point here is that we can use many examples with same logic from one function
 //use it over and over again
 
@@ -40,7 +47,7 @@ console.log(ageMarry);
 
 
 
-//---------------
+//------------------------------------------------------------------
 
 
 function yearsToRetirement(name, yearOfBirth) {
