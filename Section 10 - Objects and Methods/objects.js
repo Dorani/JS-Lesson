d4da5348bb78e99b7def0cb8f59a68d0787f6d08
Seif <seif.dorani@gmail.com>
2018-03-29 00:54:17 -0700

    next section on objects

---
+++ Section 10 - Objects and Methods/objects.js
@@ -0,0 +1,16 @@
+//Objects and methods
+
+
+var person = {
+  name: 'john',
+  lastName: 'smith',
+  year: 1990,
+  job: 'teacher',
+  isMarried: false
+};
+
+//What else can objects hold?
+  //other objects
+  //Arrays
+  //functions
+  
