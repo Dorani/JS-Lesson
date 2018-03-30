fd6586e0aaacd92405af3f35e0dd671ac21f64a4
Seif <seif.dorani@gmail.com>
2018-03-29 22:16:57 -0700

    edits, typos, and go an array in the person object, followed by a function

--- Section 10 - Objects and Methods/objects.js
+++ Section 10 - Objects and Methods/objects.js
@@ -1,18 +1,33 @@
 //Objects and methods
 
+
+function calculateAge(yearOfBirth){
+  var age = 2018 - yearOfBirth;
+  return age;
+}
+var ageJohn = calculateAge(1990);
+var ageMike = calculateAge(1967);
+var ageMarry = calculateAge(1877);
+console.log(ageJohn);
+console.log(ageMike);
+console.log(ageMarry);
+
+
+
+
 //strings numbers and booleans
 var person = {
   name: 'john',
   lastName: 'smith',
   year: 1990,
   job: 'teacher',
-  isMarried: false
+  isMarried: false,
   family: ['jane', 'mark', 'sammy', 'kenny', 'james'],//array of person john's family
   calculateAge: function(yearOfBirth){//function expression, inside of an object, since we have key value pairs we do not have the assignment opperator(=) but we have :
-
+      return 2018 -  yearOfBirth;
   }
 };
-
+console.log(person);
 //What else can objects hold?
   //other objects
   //Arrays
