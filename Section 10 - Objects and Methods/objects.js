d5710f94923f6ffd41d53ce6a796aa5f0364bc24
Seif <seif.dorani@gmail.com>
2018-03-29 22:05:17 -0700

    edits: '

--- Section 10 - Objects and Methods/objects.js
+++ Section 10 - Objects and Methods/objects.js
@@ -1,16 +1,17 @@
 //Objects and methods
 
-
+//strings numbers and booleans
 var person = {
   name: 'john',
   lastName: 'smith',
   year: 1990,
   job: 'teacher',
   isMarried: false
+  family: ['jane', 'mark', 'sammy', 'kenny', 'james'],//array of person john's family
+  
 };
 
 //What else can objects hold?
   //other objects
   //Arrays
   //functions
-  
