b3d6517dc5cd70f38ac37a20f7847aac3fcf0928
Seif <seif.dorani@gmail.com>
2018-04-05 22:15:58 -0700

    break

--- Section 11 - Loops and Iteration/while.js
+++ Section 11 - Loops and Iteration/while.js
@@ -8,3 +8,18 @@ while (i < names.length){//while loops only has the contidion that is evaluated 
   console.log(name[i]);
   i++;//update counter
 }
+
+
+//statements for Loops
+  //break: to finish a loop at a certain point
+  //continue: quit curent iteration and continue with next one
+
+for ( var i = 1; i <= 5; i++){
+  console.log(i);
+
+  if ( i === 3){
+    break;//prints 1 2 and 3, then breaks the loop
+  }
+}
+
+//want to stop this loop up to 3
