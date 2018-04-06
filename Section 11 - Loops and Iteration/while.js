0c63521c957c2a703f2f9a3de48d5167ed0e5525
Seif <seif.dorani@gmail.com>
2018-04-05 22:11:46 -0700

    while loop

---
+++ Section 11 - Loops and Iteration/while.js
@@ -0,0 +1,10 @@
+//while Loops
+
+var names = ['seif','bane', 'same', 'jeff','bob'];//arrays have properties
+
+//print these out
+var i = 0;//counter, before the loop starts
+while (i < names.length){//while loops only has the contidion that is evaluated for each loop
+  console.log(name[i]);
+  i++;//update counter
+}
