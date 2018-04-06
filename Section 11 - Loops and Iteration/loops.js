64fe3dc3af27b0aef0f876f7c34386a323c10c27
Seif <seif.dorani@gmail.com>
2018-04-05 21:51:52 -0700

    loops

---
+++ Section 11 - Loops and Iteration/loops.js
@@ -0,0 +1,20 @@
+//Loops
+
+//Control structures:
+//Imagine you have a repetitive task, instead of writing 10 lines
+//of the same code, you can just use a loop:
+
+
+//Imagine we want to print to the console, the number 0 - 9
+
+
+//1.start we the intial value of a counter
+    //counter = variable that gets updated after each iteration of the Loops
+    //we call it i and we start it at 0
+
+//2.condition that is evaluated after each loop iteration
+//designed to stop the loop at a certain condition
+
+//3. Updated to the counter
+//we simply increase i by 1
+for (var i = 0; i < 10; i++)
