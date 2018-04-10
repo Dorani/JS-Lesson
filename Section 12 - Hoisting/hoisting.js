ac794dcf113bd38261d597cdbe566517804a136a
Seif <seif.dorani@gmail.com>
2018-04-10 01:08:51 -0700

    hoisting theory on func declaration example

--- Section 12 - Hoisting/hoisting.js
+++ Section 12 - Hoisting/hoisting.js
@@ -2,9 +2,22 @@
 
 calculateAge(1965);
 
-
+//function declaration:
 function calculateAge(year) {
-  console.log(2016 - year);
+  console.log(2018 - year);
 }
 
 //calculateAge(1999);
+
+//Behind the scenes:
+//In the creation phase of the execution context: Global execution context:
+//The function declaration calculateAge is stored in the variable object, before the code is executed
+//This is why when we then enter the execution phase, the calculateAge function is already available to us
+//to use it so we do not need to declare it.
+
+
+//function expression:
+
+var retirement = function(year){
+  console.log(65 - (2018 - year));
+}
