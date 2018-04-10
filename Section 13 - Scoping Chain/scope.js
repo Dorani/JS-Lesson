bce43f939efc7d7fae27c2320f9bae0856229821
Seif <seif.dorani@gmail.com>
2018-04-10 01:33:27 -0700

    scoping

---
+++ Section 13 - Scoping Chain/scope.js
@@ -0,0 +1,10 @@
+//----------------- SCOPING IN JAVASCRIPT -------------//
+
+//Scoping answers the question "where can we access a certain varibale?"
+//Or function.
+
+//In JS, each function creates a scope:
+    // a space-environment, in which the variables it defines are accesible
+//In order to create a new scope, a new function needs to be declared
+
+//Lexical scoping:
