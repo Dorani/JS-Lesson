e14d9347770e05536896970f657dbe55949723b9
Seif <seif.dorani@gmail.com>
2018-04-10 17:28:00 -0700

    edits

--- JS Section 13 - Scoping Chain/global_scope.js
+++ JS Section 13 - Scoping Chain/global_scope.js
@@ -1,12 +1,12 @@
-var a = 'hello';
-first();
+var a = 'hello';//global scope
+first();//global scope
 
 function first() {
-  var b = 'hi!';
+  var b = 'hi!';//first() scope + variable object global
   second();
 
   function second(){
     var c = 'hey!';
     console.log( a + ' ' + b + ' ' + c)
-  }
+  }//local scope
 }
