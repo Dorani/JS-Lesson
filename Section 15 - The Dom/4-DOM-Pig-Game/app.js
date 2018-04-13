d3c2e23325c60872e1e1a60222af70870f1adc56
Seif <seif.dorani@gmail.com>
2018-04-13 00:44:10 -0700

    inner html and text content method

--- Section 15 - The Dom/4-DOM-Pig-Game/app.js
+++ Section 15 - The Dom/4-DOM-Pig-Game/app.js
@@ -15,13 +15,20 @@ GAME RULES:
 //How to manipulate the Dom
 //How to read from the Dom
 //How to change css styles
+//---------------------------------------------//
 
-
+//How to create our fundamental game variables
 var scores = [0,0]//setting both players scores to 0
 var roundScore = 0;//round score
 var activePlayer = 0;//0 will be the first player, 1 will be the second
 
-
+//How to generate a random number
 var dice = Math.floor(Math.random() * 6) + 1;//generate a random number between 1 and 6, remove decimals by using the floor method
-
 console.log(dice);
+
+//How to manipulate the Dom
+document.querySelector('#current-' + activePlayer).textContent = dice;//selects stuff exactly the way we do in css, we selected the score-0 element
+//when we change the active player, the score will be printed in the other label
+
+//Another way: putting html in the selected element, we use the inner html method instead of textContent
+document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';//writing the html needs to be a string, so the parser understands its html code
