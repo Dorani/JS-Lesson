8ee7264a1a37625075a10fc1582a10523bf96d0c
Seif <seif.dorani@gmail.com>
2018-04-12 22:12:33 -0700

    theory on this

---
+++ Section 14 - This/this.js
@@ -0,0 +1,10 @@
+console.log('we live');
+
+
+//The this variables
+  //Regular function call: the this keyword points at the global objet(the window object in the browser)
+
+  //Method call: (function attached to an object) : the this variable points to the object that is calling the Method
+
+  //The this keyword is not assigned a value until a function where it is defined is actually called
+  //It is attached to the execution context, which is only created as soon as a function is invoked(called)
