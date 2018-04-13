27c30ea583fabafdc0ea0f5ae1aff327f0b172c9
Seif <seif.dorani@gmail.com>
2018-04-12 22:17:03 -0700

    demo on this on a regular function, showing that this refers to the window object in this instance

--- Section 14 - This/this.js
+++ Section 14 - This/this.js
@@ -1,6 +1,3 @@
-console.log('we live');
-
-
 //The this variables
   //Regular function call: the this keyword points at the global objet(the window object in the browser)
 
@@ -8,3 +5,18 @@ console.log('we live');
 
   //The this keyword is not assigned a value until a function where it is defined is actually called
   //It is attached to the execution context, which is only created as soon as a function is invoked(called)
+
+
+console.log(this);//the default object, with many properties
+
+//This in action
+
+calculateAge(1985);
+
+function calculateAge(year){
+  console.log(2016 - year);
+  console.log(this);//once again it is the window object, why? because this is a regular function call which points to the window object
+}
+//this function is attached to the global object
+
+//------------------------------------//
