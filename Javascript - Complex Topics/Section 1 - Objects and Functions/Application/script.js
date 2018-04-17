a12f05d648198e6bb8be11bd55fcd930d703c159
Seif <seif.dorani@gmail.com>
2018-04-17 10:16:29 -0700

    attaching methods to the constructor functions prototype property

--- Javascript - Complex Topics/Section 1 - Objects and Functions/Application/script.js
+++ Javascript - Complex Topics/Section 1 - Objects and Functions/Application/script.js
@@ -9,17 +9,17 @@ var Person = function(name, yearOfBirth, job){
   this.name = name;
   this.yearOfBirth = yearOfBirth;
   this.job = job;
-  this.calculateAge = function(){
-    console.log(2018 - this.yearOfBirth);
-  }
 }
 
+//Prototype:
 
+Person.prototype.calculateAge = function(){//simply a property and then we add our function
+  console.log(2018 - this.yearOfBirth);
+}
 
 //instances of the person object
 //intantiation:
 var john = new Person('john', 1990, 'teacher');//how to create new objects using the constructor functions
-john.calculateAge();
 //new operator create an empty object, then the custrutor function (person) is called with arguments specified.
   //calling a function creates a new "execution context"
   //that has a "this" variable is:
@@ -28,8 +28,14 @@ john.calculateAge();
 
 
 //create more objects for more people:
-
+var john = new Person('john', 1990, 'teacher');//how to create new objects using the constructor functions
 var jane = new Person('jane', 1969, 'designer');
 var mark = new Person('mark', 1948, 'retired');
 
-//Now, if we run this, each of these objects have the calculateAge method attached
+john.calculateAge();
+jane.calculateAge();
+mark.calculateAge();
+
+//Again, the method, is not in the constructor, and we can still use it!
+//Because it is in the prototype property of our function constructor
+//This is inheretence in practice
