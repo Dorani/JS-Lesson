a87e278ca2eb9df17e61c1f79ef1a7288e47dcad
Seif <seif.dorani@gmail.com>
2018-04-17 10:11:27 -0700

    added method calculateage

--- Javascript - Complex Topics/Section 1 - Objects and Functions/Application/script.js
+++ Javascript - Complex Topics/Section 1 - Objects and Functions/Application/script.js
@@ -9,14 +9,27 @@ var Person = function(name, yearOfBirth, job){
   this.name = name;
   this.yearOfBirth = yearOfBirth;
   this.job = job;
+  this.calculateAge = function(){
+    console.log(2018 - this.yearOfBirth);
+  }
 }
+
+
 
 //instances of the person object
 //intantiation:
 var john = new Person('john', 1990, 'teacher');//how to create new objects using the constructor functions
-
+john.calculateAge();
 //new operator create an empty object, then the custrutor function (person) is called with arguments specified.
   //calling a function creates a new "execution context"
   //that has a "this" variable is:
   //pointing to the empty object created with the new operator
   //assigned to the john variable
+
+
+//create more objects for more people:
+
+var jane = new Person('jane', 1969, 'designer');
+var mark = new Person('mark', 1948, 'retired');
+
+//Now, if we run this, each of these objects have the calculateAge method attached
