557358c55380285f5cf12647049cb4cb1df10744
Seif <seif.dorani@gmail.com>
2018-04-18 12:30:42 -0700

    example of bind

--- Javascript - Complex Topics/Objects and Functions/8.Bind, Call and Apply/script.js
+++ Javascript - Complex Topics/Objects and Functions/8.Bind, Call and Apply/script.js
@@ -51,3 +51,29 @@ johnFriendly('night');
 
 var emilyFormal = john.presentation.bind(emily,'formal');
 emilyFormal('afternoon');
+
+
+//------------------------------------EXAMPLE-----------------------------------------------------//
+var years = [1990, 1965, 1937, 2005, 1998];
+
+//a machine that accepts a function then applies that function to all elements of an array
+function arrayCalc(arr, fn){
+  var arrRes = [];
+  for ( var i = 0; i < arr.length; i++){
+    arrRes.push(fn(arr[i]));
+  }
+  return arrRes;
+}
+
+function calculateAge(el) {
+  return 2016 - el;
+}
+
+function isFullAge(limit,el) {
+  return el >= limit;
+}
+
+var ages = arrayCalc(years, calculateAge);
+var fullJapan = arrayCalc(ages, isFullAge.bind(this, 20));
+console.log(ages);
+console.log(fullJapan);
