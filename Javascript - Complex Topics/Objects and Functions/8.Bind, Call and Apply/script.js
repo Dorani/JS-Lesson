a4802c1009b5d26677671b6318aff3118ed4a4bd
Seif <seif.dorani@gmail.com>
2018-04-18 12:09:01 -0700

    call method in action

--- Javascript - Complex Topics/Objects and Functions/8.Bind, Call and Apply/script.js
+++ Javascript - Complex Topics/Objects and Functions/8.Bind, Call and Apply/script.js
@@ -1,1 +1,32 @@
-//Bind, call and apply
+//Bind, call and apply methods:
+
+//Allow us to call a function and set the this variable manually
+
+
+var john = {
+  name: 'john',
+  age: 26,
+  job: 'teacher',
+  presentation: function(style, timeOfDay){
+    if (style === 'formal'){
+      console.log('Good ' + timeOfDay + ', ladies and gentlemen! Im ' +
+          this.name + ' and Im a ' + this.job + ' . Im ' + this.age + ' years old.');
+    } else if (style === 'friendly'){
+      console.log('Hey, whats up, Im' + this.name + 'Im a ' + this.job + 'and Im ' + this.age + 'years old' +timeOfDay +' ');
+    }
+  }
+};
+
+var emily = {
+  name: 'emily',
+  age: 35,
+  job: 'designer'
+};
+
+
+john.presentation('formal', 'morning');
+
+//using the call method it allows the seting the this var to emily, so we can use johns method
+//method borrowing:
+john.presentation.call(emily, 'friendly', 'afternoon');//emily and it works!
+//we borrowed the method from john to use on the emily object
