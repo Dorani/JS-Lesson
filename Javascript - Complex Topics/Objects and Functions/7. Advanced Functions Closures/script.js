8b82d3624a09c36461c58b69ce55ff7179623b81
Seif <seif.dorani@gmail.com>
2018-04-18 10:22:54 -0700

    the power of closures

--- Javascript - Complex Topics/Objects and Functions/7. Advanced Functions Closures/script.js
+++ Javascript - Complex Topics/Objects and Functions/7. Advanced Functions Closures/script.js
@@ -13,7 +13,37 @@ function retirement(retirementAge){//pass into it the age at which someone can o
 var retirementUS = retirement(66);
 retirementUS(1990);//42 years left until retirement
 //or
-retirement(66)(1990);//42 years left until retirement
+//retirement(66)(1990);//42 years left until retirement
 
 //Our inner function, is able to use the retirement variable and a variable of the "retirement" function that is already returned
 //this is what a closure is!
+
+
+var retirementGermany = retirement(65);
+var retirementIceland = retirement(67);
+
+retirementGermany(1990);
+retirementIceland(1990);
+
+
+//--------------------------------------------------------------------
+
+function interviewQuestion(job){
+  return function(name) {
+    if ( job === 'designer'){
+      console.log(name + ' , can you please explain what UX is?');
+    } else if (job === 'teacher'){
+      console.log('what subject do you teach,' + name + '?');
+    } else {
+      console.log('hello' + name + ', what do you do?');
+    }
+  }
+}
+
+interviewQuestion(teacher)('john');//what subject do you teach john?
+//this function call will return the function(name)
+//then we call the function with the john parameter
+// and the execution context will close in over the variable object over the function we called before, var defined ie job var,
+//we can use job var in our if else  in our inner function
+
+//the power of closure! much cleaner
