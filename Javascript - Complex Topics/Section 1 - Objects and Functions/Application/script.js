50bc46c89f781830754733133e5ad2a4416ad270
Seif <seif.dorani@gmail.com>
2018-04-17 17:31:35 -0700

    primitives vs objects

--- Javascript - Complex Topics/Section 1 - Objects and Functions/Application/script.js
+++ Javascript - Complex Topics/Section 1 - Objects and Functions/Application/script.js
@@ -78,3 +78,55 @@ var jane = Object.create(personProto,
 
 //Obj.create builds in obj that inherets directly from the one we passed in the 1st argument
 //Function constructor, the newly created obj inherest from the custrutor prototype property
+
+
+
+
+//---------------------------------------------------------------------------
+//Primitives vs Objects:
+
+//Variables containing primitives, hold that data in themselves, but in objects they contain a reference in a place in memory:
+//Primitives:
+var a = 23;
+var b = a;
+
+a = 46;
+
+console.log(a);//46
+console.log(b);//23
+//we copied the value of a to b, and when we mutated a, it didnt affect b
+
+
+//Now with objects:
+var obj1 = {
+  name: 'john',
+  age: 2
+}
+
+var obj2 = obj1;
+obj1.age = 30;
+console.log(obj1.age);//30
+console.log(obj2.age);//30
+//when we set obj1=obj2 we just created a new reference which points to the 1st object
+
+
+//Now with functions:
+var age = 27;
+var obj = {
+  name:'seif',
+  city: 'sf'
+};
+
+function change(a,b){
+  a = 30;
+  b.city = 'new york';
+}
+
+change(age,obj);
+console.log(age);//27
+console.log(obj.city);//new york
+
+//same thing
+//we passed the age variable holding a primitive and object var holding a reference to an object into our function
+//this function then, as it was invoked, attempted to change the arguments that we passed into it
+//when we console log the value, we see the primitive is unchanged, but city goes from sf to new york
