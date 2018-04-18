82c6125a5303108cd3581a8b87479e3a6b8f7119
Seif <seif.dorani@gmail.com>
2018-04-18 09:26:25 -0700

    more on iife

--- Javascript - Complex Topics/Objects and Functions/6.Advanced Functions (part3)/script.js
+++ Javascript - Complex Topics/Objects and Functions/6.Advanced Functions (part3)/script.js
@@ -10,7 +10,10 @@ game();//false
 //creating a private variable
 //we should use an iife :
 
-(function() {//in js what is inside parenthesis, is treated as an expression
-  var score = Math.random() * 10;
+(function(goodLuck) {//in js what is inside parenthesis, is treated as an expression
+  var score = Math.random() * 10;//data privacy
   console.log(score >= 5);
-})();
+})(5);
+//true
+//all we want is to create a new scope hidden from outside scope
+//we dont interfere with other variables in our global execution context
