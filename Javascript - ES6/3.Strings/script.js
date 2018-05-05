d5aad22f824b84381b9c52484939947ce1f1c6cc
Seif <seif.dorani@gmail.com>
2018-05-04 21:57:01 -0700

    outputs

--- Javascript - ES6/3.Strings/script.js
+++ Javascript - ES6/3.Strings/script.js
@@ -14,6 +14,7 @@ function calcAge(year){
 console.log('This is ' + firstName + ' ' + lastName +
 '. He was born in ' + yearOfBirth + ', today he is ' +
  calcAge(yearOfBirth) + ' years old.');
+ //This is John Smith. He was born in 1990, today he is 26 years old.
 
  //This is long to write with all the + signs etc....
 //----------------------//
@@ -22,3 +23,5 @@ console.log('This is ' + firstName + ' ' + lastName +
 //Allows you to write the text within the backticks and the variables!
  console.log(`This is ${firstName} ${lastName}. He was born in
  ${yearOfBirth} and today he is ${calcAge(yearOfBirth)} years old`);
+//This is John Smith. He was born in
+//1990 and today he is 26 years old
