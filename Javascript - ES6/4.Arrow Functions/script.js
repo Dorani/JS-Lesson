d0b217d4fa7b5f5b38ff52cf8c05535e7df2e59d
Seif <seif.dorani@gmail.com>
2018-05-04 22:12:11 -0700

    map method in es5

--- Javascript - ES6/4.Arrow Functions/script.js
+++ Javascript - ES6/4.Arrow Functions/script.js
@@ -2,3 +2,14 @@
 
 //IE: Array with birth years and we want to calculate the age for these
 //birth years
+
+const years = [1990, 1965, 1982, 1937];
+
+//we can use the map method to loop over the array and then do stuff with it
+
+
+//ES5
+var ages5 = years.map(function(el){
+  return 2016 - el;
+});
+console.log(ages5);//26,51,34,79
