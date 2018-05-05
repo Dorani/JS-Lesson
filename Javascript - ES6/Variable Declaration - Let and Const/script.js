858461caa924888c2fa53e2b72c4c4fb51cb27e1
Seif <seif.dorani@gmail.com>
2018-05-04 21:33:22 -0700

    more on scopes

--- Javascript - ES6/Variable Declaration - Let and Const/script.js
+++ Javascript - ES6/Variable Declaration - Let and Const/script.js
@@ -19,7 +19,7 @@ console.log(name6)//error - if we attempt to change the value of a constant
 
 
 //ES5
-function driversLicense(passedTest) {
+function driversLicense5(passedTest) {
   if (passedTest) {
     var firstName = 'John';
     var yearOfBirth = 1990;
@@ -28,4 +28,23 @@ function driversLicense(passedTest) {
   }
 }
 
-driversLicense(true);
+driversLicense5(true);//John, born in 1990, and can drive!
+
+
+//ES6
+function driversLicense6(passedTest) {
+  if (passedTest) {
+    let firstName = 'John';
+    const yearOfBirth = 1990;
+
+    console.log(firstName + ' born in ' + yearOfBirth + ', and can drive!');
+  }
+}
+
+driversLicense6(true);//John, born in 1990, and can drive!
+
+//Must remember here that let and const the variables are not function scoped
+//but they are 'blocked scoped', within the curly braces, the var declared with let
+//and const are are only accesible within inside the block
+//also if you define the const and let and then define the value inside, you can with let
+//but not const!
