e07e66f3202ffa575d1a6a26582dd858b6360eda
Seif <seif.dorani@gmail.com>
2018-05-04 21:44:35 -0700

    edits

--- Javascript - ES6/2.Blocks and IIFES/script.js
+++ Javascript - ES6/2.Blocks and IIFES/script.js
@@ -1,6 +1,6 @@
 //Blocks and IIFES
 //Achieving data privacy
-
+//ES6
 {
   const a = 1
   let b = 2;
@@ -9,3 +9,11 @@
 console.log(a + b);//error
 //these values are not accesible from outside the block
 //like an iife!
+
+
+
+//es5
+(function(){
+  var c =3;
+})();
+console.log(c);//also an error
