20bf042a9f8faa9391d028dcbd8796f690854dc0
Seif <seif.dorani@gmail.com>
2018-05-04 21:57:47 -0700

    edits

--- Javascript - ES6/3.Strings/script.js
+++ Javascript - ES6/3.Strings/script.js
@@ -10,16 +10,18 @@ function calcAge(year){
 }
 
 //Template literals:
+
 //ES5:
 console.log('This is ' + firstName + ' ' + lastName +
 '. He was born in ' + yearOfBirth + ', today he is ' +
  calcAge(yearOfBirth) + ' years old.');
  //This is John Smith. He was born in 1990, today he is 26 years old.
+//This is long to write with all the + signs etc....
 
- //This is long to write with all the + signs etc....
 //----------------------//
 
-//In ES6 we have template literals, ie we use backticks:
+//In ES6:
+//We have template literals, ie we use backticks:
 //Allows you to write the text within the backticks and the variables!
  console.log(`This is ${firstName} ${lastName}. He was born in
  ${yearOfBirth} and today he is ${calcAge(yearOfBirth)} years old`);
