de1ab689b68b6c3bb6a54328f94486ec66058603
Seif <seif.dorani@gmail.com>
2018-05-04 21:21:55 -0700

    es6 vs es5 const and let

---
+++ Javascript - ES6/Variable Declaration - Let and Const/script.js
@@ -0,0 +1,18 @@
+//ES5
+
+var name5 = 'Jane Smith';
+var age5 = 23;
+
+name5 = 'Jane Miller';
+console.log(name5);//Jane Miller to demo mutation
+
+//ES6
+//-- 2 Methods here, const and let - const meaning constant for values
+//-- we do not want to change, and let is like the old var - values we mutatate
+
+const name6 = 'Jane Smith';
+let age = 23; //since this value can mutate/change
+
+name6 = 'Jane Miller';
+console.log(name6)//error - if we attempt to change the value of a constant
+//it will throw an error - Assignment to constant variable...
