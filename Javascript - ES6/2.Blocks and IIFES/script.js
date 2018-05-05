349ef828f512c2fdc4e80133be52021f5ede0915
Seif <seif.dorani@gmail.com>
2018-05-04 21:42:38 -0700

    example of iife in es6

---
+++ Javascript - ES6/2.Blocks and IIFES/script.js
@@ -0,0 +1,11 @@
+//Blocks and IIFES
+//Achieving data privacy
+
+{
+  const a = 1
+  let b = 2;
+}
+
+console.log(a + b);//error
+//these values are not accesible from outside the block
+//like an iife!
