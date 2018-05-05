65af1dc90184af4959d524719633908095d395d6
Seif <seif.dorani@gmail.com>
2018-05-04 21:46:46 -0700

    the diff with var

--- Javascript - ES6/2.Blocks and IIFES/script.js
+++ Javascript - ES6/2.Blocks and IIFES/script.js
@@ -1,18 +1,21 @@
 //Blocks and IIFES
 //Achieving data privacy
+
 //ES6
 {
   const a = 1
   let b = 2;
+  var c = 3;
 }
 
 console.log(a + b);//error
 //these values are not accesible from outside the block
 //like an iife!
+console.log(c)//3 - Because with var declarations, it doesnt matter if there inside of a block
+//or if they are not, because they are 'function scoped'
 
-
-
-//es5
+//----------------//
+//ES5
 (function(){
   var c =3;
 })();
