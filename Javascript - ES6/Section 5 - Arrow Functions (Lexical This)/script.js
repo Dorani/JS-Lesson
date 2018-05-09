9e58979eceffaac9a3d08c895c4188fcc94517b4
Seif <seif.dorani@gmail.com>
2018-05-09 13:46:41 -0700

    diff between what happens with the this keyword in a method and in a func, access to scopes etc...

--- Javascript - ES6/Section 5 - Arrow Functions (Lexical This)/script.js
+++ Javascript - ES6/Section 5 - Arrow Functions (Lexical This)/script.js
@@ -18,3 +18,10 @@ box5.clickMe();
 //Only method call with a this keyword, will point to that object
   //However, a regular function call, the this keyword will always point
   //to the global obj
+
+
+//In the method (function attached to an object) on line 6 we have access to color and position
+//But, the callback function inside the event handler, is not a method:
+  //its a regular function call, so the this keyword, points at the window global object
+  //and so, the position and color attributes are not defined in the window object
+  //so..... we get undefined.
