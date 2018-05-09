f5bf3d54f0175c0d2bffadaf4f7fa6c9d03585f8
Seif <seif.dorani@gmail.com>
2018-05-09 13:31:55 -0700

    more comprehensive, 3 ways of writing arrow functions complete

--- Javascript - ES6/Section 4 - Arrow Functions/script.js
+++ Javascript - ES6/Section 4 - Arrow Functions/script.js
@@ -23,7 +23,13 @@ let ages6 = years.map(el => 2016 - el);
 //no function keyword, no return, not braces, its that simple!!
 console.log(ages6);//26,51,34,79
 
-ages6 = years.map((el, index) => `Age element ${index + 1}: ${2016 - el}.`);
+ages6 = years.map((el, index) => `Age element ${index + 1}: ${2018 - el}.`);
 console.log(ages6);
 //logs the index and the value with a string, this is what we have to do if
 //we have 2 arguments
+
+ages6  = years.map((el, index) => {
+  const now = new Date().getFullYear();
+  const age = now - el;
+  return `Age element ${index + 1}: ${age}.`;
+});
