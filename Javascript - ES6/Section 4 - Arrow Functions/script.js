25910f7cdad7bb83754b54c3dd0c1c5b82b578da
Seif <seif.dorani@gmail.com>
2018-05-09 13:33:33 -0700

    cleaning up lesson plan

--- Javascript - ES6/Section 4 - Arrow Functions/script.js
+++ Javascript - ES6/Section 4 - Arrow Functions/script.js
@@ -2,9 +2,7 @@
 
 //IE: Array with birth years and we want to calculate the age for these
 //birth years
-
 const years = [1990, 1965, 1982, 1937];
-
 //we can use the map method to loop over the array and then do stuff with it
 
 
@@ -13,8 +11,10 @@ var ages5 = years.map(function(el){
   return 2016 - el;
 });
 console.log(ages5);//26,51,34,79
-
+//-----------------------------------------------------
 //ES6
+                      //First Way:
+
 let ages6 = years.map(el => 2016 - el);
 //el is the argument
 //=> operator
@@ -23,11 +23,15 @@ let ages6 = years.map(el => 2016 - el);
 //no function keyword, no return, not braces, its that simple!!
 console.log(ages6);//26,51,34,79
 
+
+                      //Second way:
 ages6 = years.map((el, index) => `Age element ${index + 1}: ${2018 - el}.`);
 console.log(ages6);
 //logs the index and the value with a string, this is what we have to do if
 //we have 2 arguments
 
+
+                      //Third Way:
 ages6  = years.map((el, index) => {
   const now = new Date().getFullYear();
   const age = now - el;
