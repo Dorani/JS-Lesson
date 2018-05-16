e76463ea4a3c5486ab45e578ab3b106fd80117a6
Seif <seif.dorani@gmail.com>
2018-05-15 18:11:33 -0700

    beg of showing how impractivle this is with bigger sets of data

---
+++ Javascript - ES6/Section 6 - Destructuring/script.js
@@ -0,0 +1,14 @@
+//Destructuring:
+
+//Convenient way to extract data from a data structure like an object
+//or an array.
+
+//Imagine we had an array stored with data and say we want to store
+//each of the elements of that array in a single variable
+
+
+//ES5:
+
+var john = ['john', 26];
+var name = john[0];
+var age = john[1];
