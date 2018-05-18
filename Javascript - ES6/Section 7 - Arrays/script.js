48c6dfd8d80b58893c6891673fe96916275e79d1
Seif <seif.dorani@gmail.com>
2018-05-17 21:30:04 -0700

    manipulating the boxes with es6

---
+++ Javascript - ES6/Section 7 - Arrays/script.js
@@ -0,0 +1,7 @@
+//Select the boxes
+
+
+//isolate those elements with that box class name to select them all
+const  boxes = document.querySelectorAll('.box');
+//returns a node list
+//so we need to transform it into an array
