0c04fba5583e1aaa3abb4fd7b17ff4cf1534904f
Seif <seif.dorani@gmail.com>
2018-05-17 21:33:31 -0700

    es5 array manip example

--- Javascript - ES6/Section 7 - Arrays/script.js
+++ Javascript - ES6/Section 7 - Arrays/script.js
@@ -5,3 +5,12 @@
 const  boxes = document.querySelectorAll('.box');
 //returns a node list
 //so we need to transform it into an array
+
+//we called the slice method on the array prototype and setting the 'this variable'
+//to the boxes in this case
+
+                      //ES5:
+
+var boxesArr5 = Array.prototype.slice.call(boxes);
+//now we have an Array
+//so we can use all the array methods
