273cbe710f364fbf4d0daf0510ed852ea6d8b805
Seif <seif.dorani@gmail.com>
2018-05-23 11:47:00 -0700

    es5 way

--- Javascript - ES6/Section 7.2 - Spread Operator/script.js
+++ Javascript - ES6/Section 7.2 - Spread Operator/script.js
@@ -10,3 +10,13 @@ function addFourAges(a,b,c,d) {
 }
 var sum1 = addFourAges(18, 30, 12, 21);
 console.log(sum1);//81
+
+
+//Imagine we had an array and not a var
+//How can we pass that entire array into the function
+
+                    //ES5
+
+var ages = [18, 30, 12, 21];
+var sum2 = addFourAges.apply(null, ages);//apply will take the array and call this function using the ages 2 elements
+console.log(sum2);//81
