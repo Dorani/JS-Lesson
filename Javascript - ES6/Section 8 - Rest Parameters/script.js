b6ceb3dfc0e4a2e2486441969082730d475e096e
Seif <seif.dorani@gmail.com>
2018-05-23 12:32:54 -0700

    es5

--- Javascript - ES6/Section 8 - Rest Parameters/script.js
+++ Javascript - ES6/Section 8 - Rest Parameters/script.js
@@ -11,15 +11,19 @@
 
 
 //Example: function that receives an arbritratry number of years and then prints
-//to the console whether each person corresponding to these years is greater than 21
+//to the console whether each person corresponding to these years is greater than 18 or fullage/adult
 
 
                           //ES5
 function isFullAge5() {
-  console.log(arguments);
+  console.log(arguments);//arguments is an 'array' like structure but NOT an array
   //transforing arguments into an array with this es5 hack
   var argsArr = Array.prototype.slice.call(arguments);
+  //loop through array and calculate if each person is greater than 18 or full age/adult
+  argsArr.forEach(function(cur){
+    console.log((2018 - cur ) >= 18);
+  })
 }
 
 isFullAge5(1990, 1999, 1965);//1990, 1999, 1965 = arguments variable
-//arguments is an 'array' like structure but NOT an array
+//TRUE, FALSE, TRUE
