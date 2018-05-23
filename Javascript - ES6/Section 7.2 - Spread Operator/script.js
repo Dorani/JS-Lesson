da49f9ab48cc18a026885f030e4c2db2526ef735
Seif <seif.dorani@gmail.com>
2018-05-23 11:35:30 -0700

    beg of example

--- Javascript - ES6/Section 7.2 - Spread Operator/script.js
+++ Javascript - ES6/Section 7.2 - Spread Operator/script.js
@@ -1,3 +1,9 @@
 //The spread Operator:
   //Convenient way to expand elements of an array in places like arguments
   //and function calls
+
+function addFourAges(a,b,c,d) {
+  return a + b + c + d;
+}
+var sum1 = addFourAges(18, 30, 12, 21);
+console.log(sum1);//81
