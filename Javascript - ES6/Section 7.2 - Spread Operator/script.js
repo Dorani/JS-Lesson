45ceb3ccea1762268558f519a8cd59409bb23a2f
Seif <seif.dorani@gmail.com>
2018-05-23 11:49:33 -0700

    instead of using the apply method with 2 parameters. we just use the spread operator

--- Javascript - ES6/Section 7.2 - Spread Operator/script.js
+++ Javascript - ES6/Section 7.2 - Spread Operator/script.js
@@ -15,8 +15,14 @@ console.log(sum1);//81
 //Imagine we had an array and not a var
 //How can we pass that entire array into the function
 
-                    //ES5
+                    //ES5:
 
 var ages = [18, 30, 12, 21];
 var sum2 = addFourAges.apply(null, ages);//apply will take the array and call this function using the ages 2 elements
 console.log(sum2);//81
+
+                    //ES6:
+const sum3 = addFourAges(...ages);//expanding
+console.log(sum3)//81
+//this spread operator expands the array into its components
+// ... = 18,30,12,21
