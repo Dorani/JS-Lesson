d50c1b1d960f0965e9f0b458dee0b4b9c4db3871
Seif <seif.dorani@gmail.com>
2018-06-12 10:41:48 -0700

    es5 example of default parameters using a constructor func

---
+++ Javascript - ES6/Section 8.1 - Default Parameters/script.js
@@ -0,0 +1,15 @@
+//Default Parameters
+  //We use them whenever we want one or more parameters of a functions
+  //to be preset - ie: default values
+
+
+//ES5 - STYLE:
+//constructor function that takes 4 parameters
+//
+function SmithPerson(firstName, yearOfBirth,
+   lastName, nationality) {
+     this.firstName = firstName;
+     this.yearOfBirth = yearOfBirth;
+     this.lastName = lastName;
+     this.nationality = nationality;
+   }
