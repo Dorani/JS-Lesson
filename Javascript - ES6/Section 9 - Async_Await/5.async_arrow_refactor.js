40d27df05fdacb1349f597f9c8a4caaa875983f7
Seif <seif.dorani@gmail.com>
2019-11-18 22:37:53 -0800

    async await arrow function refactor

---
+++ Javascript - ES6/Section 9 - Async_Await/5.async_arrow_refactor.js
@@ -0,0 +1,22 @@
+async function fectchAlbums() {
+  //make a request and assign it to a variable
+  const res = await fetch("https://rallycoding.herokuapp.com/api/music_albums")
+    //calling json on our res var and assigning it to json
+    const json = await res.json()
+    //then consoling out the response
+    console.log(json)
+}
+fectchAlbums();
+
+
+
+
+//Async await arrow function style refactor:
+
+const fectchAlbums = async () => {
+  const res = await fetch("https://rallycoding.herokuapp.com/api/music_albums")
+    const json = await res.json();
+
+    console.log(json)
+}
+fectchAlbums();
