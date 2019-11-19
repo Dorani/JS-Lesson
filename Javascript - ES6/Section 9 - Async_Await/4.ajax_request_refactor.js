e4de0c24a818641ed32a1e04ad39f07ab7e17628
Seif <seif.dorani@gmail.com>
2019-11-18 22:30:17 -0800

    breakdown of async await in action, compare and contrast with old way

--- Javascript - ES6/Section 9 - Async_Await/4.ajax_request_refactor.js
+++ Javascript - ES6/Section 9 - Async_Await/4.ajax_request_refactor.js
@@ -10,11 +10,20 @@ function fectchAlbums() {
 //Once you have identified your asynchronous code, do the following:
 
 // step 1
-//This tells our interpreter that the following function we are
-//about to declare constains async code
+//Put 'async' in front of our function.
+//This tells our interpreter that the following
+//function we are  about to declare constains async code
 
+
+//Step 2:
+//Identify all the promises that are created within that function
+//we have 2, one created by calling fetch, another by callin res.json
+//in front of those promises add 'await' and then we assign them to 2 variables
 async function fectchAlbums() {
-  fetch("https://rallycoding.herokuapp.com/api/music_albums")
-    .then(res => res.json())
-    .then(json => console.log(json))
+  //make a request and assign it to a variable
+  const res = await fetch("https://rallycoding.herokuapp.com/api/music_albums")
+    //calling json on our res var and assigning it to json
+    const json = await res.json()
+    //then consoling out the response
+    console.log(json)
 }
